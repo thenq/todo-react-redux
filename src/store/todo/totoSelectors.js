811ea1aaf8643f78580f9cd86642fd66cf14e477
thenq <thenq>
2022-08-28 19:54:44 +0700

    Install reselect package

--- src/store/todo/totoSelectors.js
+++ src/store/todo/totoSelectors.js
@@ -1,9 +1,9 @@
-import { createSelector } from 'redux-toolkit'
+import { createSelector } from 'reselect'
 
 export const getTodoList = (state) => state.todo.todoList
 export const getSearchValue = (state) => state.todo.filters.search
-export const getSearchStatus = (state) => state.todo.todoList.status
-export const getSearchPriority = (state) => state.todo.todoList.priority
+export const getSearchStatus = (state) => state.todo.filters.status
+export const getSearchPriority = (state) => state.todo.filters.priority
 
 export const getTodoListWithFilter = createSelector(
   getTodoList,
@@ -11,7 +11,6 @@ export const getTodoListWithFilter = createSelector(
   getSearchStatus,
   getSearchPriority,
   (todoList, search, status, priority) => {
-    // const { search, status, priority } = state.todo.filters
     return todoList.filter(
       (todo) =>
         (!search ? true : todo.name.includes(search)) &&
